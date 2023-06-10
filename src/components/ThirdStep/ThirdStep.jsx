d1f91b31df088409b03fd3043dd9e94fecd86876
KoMRaD124 <40596149+KoMRaD124@users.noreply.github.com>
2023-06-10 17:25:18 +0300

    Add files via upload

---
+++ src/components/ThirdStep/ThirdStep.jsx
@@ -0,0 +1,117 @@
+import React from "react";
+import styles from "./SecondStep.module.scss";
+
+import { ReactComponent as IconOne } from "../../assets/1.svg";
+import { ReactComponent as IconTwo } from "../../assets/2.svg";
+import { ReactComponent as IconThree } from "../../assets/3.svg";
+import { ReactComponent as IconFour } from "../../assets/4.svg";
+import { ReactComponent as IconFive } from "../../assets/5.svg";
+import { ReactComponent as IconSix } from "../../assets/6.svg";
+import { ReactComponent as IconSeven } from "../../assets/7.svg";
+import { ReactComponent as IconEight } from "../../assets/8.svg";
+import { ReactComponent as IconNine } from "../../assets/9.svg";
+import { ReactComponent as IconTen } from "../../assets/10.svg";
+
+import { smileSS } from "../../assets";
+
+export const ThirdStep = ({ SetName, setTel, SetStep, name, tel }) => {
+  const [click, setClick] = React.useState(false);
+
+  const [rating, setRating] = React.useState(null);
+  const [oneIsActive, setOneIsActive] = React.useState(false);
+  const [twoIsActive, setTwoIsActive] = React.useState(false);
+  const [threeIsActive, setThreeIsActive] = React.useState(false);
+  const [fourIsActive, setFourIsActive] = React.useState(false);
+  const [fiveIsActive, setFiveIsActive] = React.useState(false);
+
+  const onClickOne = () => {
+    setOneIsActive(true);
+    setRating(1);
+    setTwoIsActive(false);
+    setThreeIsActive(false);
+    setFourIsActive(false);
+    setFiveIsActive(false);
+    setClick(true);
+  };
+  const onClickTwo = () => {
+    setOneIsActive(false);
+    setRating(2);
+    setTwoIsActive(true);
+    setThreeIsActive(false);
+    setFourIsActive(false);
+    setFiveIsActive(false);
+    setClick(true);
+  };
+  const onClickThree = () => {
+    setOneIsActive(false);
+    setRating(3);
+    setTwoIsActive(false);
+    setThreeIsActive(true);
+    setFourIsActive(false);
+    setFiveIsActive(false);
+    setClick(true);
+  };
+  const onClickFour = () => {
+    setOneIsActive(false);
+    setRating(4);
+    setTwoIsActive(false);
+    setThreeIsActive(false);
+    setFourIsActive(true);
+    setFiveIsActive(false);
+    setClick(true);
+  };
+  const onClickFive = () => {
+    setOneIsActive(false);
+    setRating(5);
+    setTwoIsActive(false);
+    setThreeIsActive(false);
+    setFourIsActive(false);
+    setFiveIsActive(true);
+    setClick(true);
+  };
+
+  return (
+    <div className={styles.body}>
+      <div className={styles.header}>
+        Оцените скорость доставки
+        <img src={smileSS} className={styles.smile} alt="" />
+      </div>
+      <div className={styles.iconsArray}>
+        {oneIsActive ? (
+          <IconSix />
+        ) : (
+          <IconOne className={styles.icon} onClick={() => onClickOne()} />
+        )}
+        {twoIsActive ? (
+          <IconSeven />
+        ) : (
+          <IconTwo className={styles.icon} onClick={() => onClickTwo()} />
+        )}
+        {threeIsActive ? (
+          <IconEight />
+        ) : (
+          <IconThree className={styles.icon} onClick={() => onClickThree()} />
+        )}
+        {fourIsActive ? (
+          <IconNine />
+        ) : (
+          <IconFour className={styles.icon} onClick={() => onClickFour()} />
+        )}
+        {fiveIsActive ? (
+          <IconTen />
+        ) : (
+          <IconFive className={styles.icon} onClick={() => onClickFive()} />
+        )}
+      </div>
+      <div>
+        <button
+          disabled={!click}
+          className={styles.button}
+          onClick={() => SetStep(3)}
+        >
+          Далее
+        </button>
+      </div>
+    </div>
+  );
+};
